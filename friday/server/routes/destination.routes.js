5970192c8f401b516f90e3bbda91e6b0fc85bdfa
RASantos92 <rasbms81608@gmail.com>
2023-02-17 12:29:28 -0600

    2/17 mongoose/express/mongodb

---
+++ friday/server/routes/destination.routes.js
@@ -0,0 +1,10 @@
+const destinationController = require("../controllers/destination.controller")
+
+
+module.exports = app => {
+    app.post("/api/destinations", destinationController.create),
+    app.get("/api/destinations", destinationController.getAll),
+    app.get("/api/destinations/:id", destinationController.getOne),
+    app.delete("/api/destinations/:id", destinationController.deleteOne),
+    app.put("/api/destinations/:id", destinationController.updateOne)
+}
